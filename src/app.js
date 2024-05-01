0cfd514b904177c2959ce0ed6f09b7a4668acfc5
Shivani <sutrejashivani57@gmail.com>
2024-05-01 16:12:45 +0530

    changed api

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import cookieParser from 'cookie-parser';
 const app = express();
 
 app.use(cors({
-    origin: 'https://dribble-project-backend.onrender.com',
+    origin: 'https://celadon-mermaid-44f7ea.netlify.app',
     methods:["POST","GET"],
     credentials: true,
 }));
